bf129415af32929cb80591350bd9f1f2a4b0ed63
Pikono95 <Nodaubercies@gmail.com>
2025-11-28 09:46:43 +0100

    update

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,11 +1,3 @@
-import json
-def ask_number(rep,maxi = None,mini = None):
-    numb = int(input(rep))
-    if maxi = None and mini = None:
-        while numb < mimi or numb > maxi:
-            print("Please enter a number between ",mini," and ",maxi,".")
-            numb = int(input(rep))
-    return numb
 def ask_text(text):
     a = 0
     while a == 0 or a == None or a == '':
@@ -18,7 +10,3 @@ def ask_choice(message, option):
             a = input(message)
     return options[int(a)-1]
 f = ask_choice("Select an option:", ["Option 1", "Option 2", "Option 3"])
-def load_file(file_path):
-    with open(file_path, 'r') as file:
-        rep = json.load(file)
-    return rep
