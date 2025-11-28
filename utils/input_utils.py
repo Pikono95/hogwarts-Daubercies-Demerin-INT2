2645c75526e22cae78231a18315f50c16e484e86
Pikono95 <Nodaubercies@gmail.com>
2025-11-28 08:59:57 +0100

    ask_number v1 guez

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,1 +1,8 @@
-
+import json
+def ask_number(rep,maxi = None,mini = None)
+    numb = int(input(rep))
+    if maxi = None and mini = None:
+        while numb < mimi or numb > maxi:
+            print("Please enter a number between ",mini," and ",maxi,".")
+            numb = int(input(rep))
+    return numb
