db2f11d07e9043c840e1732e03e9888a2d20b598
LEDidems <adriendem@gmail.com>
2025-12-07 10:21:42 +0100

    display_character function

--- universe/character.py
+++ universe/character.py
@@ -7,4 +7,14 @@ def init_character():
         "Spells": [],
         "Attributes": {}
     }
-return character
+    return character
+
+def display_character(character):
+    print("Character Information:")
+    print("Name: {} {}".format(character["First Name"], character["Last Name"]))
+    print("Money: {}".format(character["Money"]))
+    print("Inventory: {}".format(", ".join(character["Inventory"]) if character["Inventory"] else "Empty"))
+    print("Spells: {}".format(", ".join(character["Spells"]) if character["Spells"] else "None"))
+    print("Attributes:")
+    for attr, value in character["Attributes"].items():
+        print("  {}: {}".format(attr, value))
