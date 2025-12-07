5ac9ce102b3aa3ce528b4403226e162832879e14
LEDidems <adriendem@gmail.com>
2025-12-07 10:10:20 +0100

    init_character v1

--- universe/character.py
+++ universe/character.py
@@ -1,1 +1,10 @@
-
+def init_character():
+    character = {
+        "Last Name": None,
+        "First Name": None,
+        "Money": 100,
+        "Inventory": [],
+        "Spells": [],
+        "Attributes": {}
+    }
+return character
