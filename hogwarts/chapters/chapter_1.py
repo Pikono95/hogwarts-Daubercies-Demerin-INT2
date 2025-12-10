9ead9e10e516ea008c5bb310cf60d36d55c314eb
Pikono95 <Nodaubercies@gmail.com>
2025-12-10 19:00:53 +0100

    update

--- hogwarts/chapters/chapter_1.py
+++ hogwarts/chapters/chapter_1.py
@@ -38,4 +38,32 @@ def recieve_letter(character):
         exit()
     
 def meet_hagrid(character):
+
+def buy_supplies(character):
+    print("Catalog of available items:")
+    catalog={
+    "1": ["Magic Wand", 35, "Galleons (required)"],
+    "2": ["Wizard Robe", 20, "Galleons (required)"],
+    "3": ["Tin Cauldron", 15, "Galleons"],
+    "4": ["Potions Book", 25, "Galleons (required)"],
+    "5": ["Magic Quill", 5, "Galleons"],
+    "6": ["Enchanted Book", 30, "Galleons"],
+    "7": ["Copper Scale", 10, "Galleons"],
+    "8": ["Invisibility Cloak", 100, "Galleons"]
+    }
+    for i in catalog:
+        print(f"- {catalog[i][0]} : {catalog[i][1]} {catalog[i][2]}")
+    while "1" not in character["Inventory"] or "2" not in character["Inventory"] or "4" not in character["Inventory"]:
+        print(f"You have {character["Money"]} Galleons.\nRemaining required items :")
+        for i in catalog:
+            if catalog[i][2] == "Galleons (required)" and i not in character["Inventory"]:
+                print(f"- {catalog[i][0]}", end='')
+        a=input("\nEnter the number of the item to buy:")
+        if character["Money"]-catalog[a][1] > 0:
+            character["Inventory"].append(a)
+            character["Money"] -= catalog[a][1]
+            print(f"You bought: {catalog[a][0]} (- {catalog[a][1]} Galleons).\n")
+        else:
+            print(f"You have {character["Money"]} Galleons. You can't buy it\n")
+
     
