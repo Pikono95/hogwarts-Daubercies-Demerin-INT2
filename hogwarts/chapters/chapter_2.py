6336a936af1d9522cf6229d275c5ae959fff8f5d
LEDidems <adriendem@gmail.com>
2025-12-12 08:54:26 +0100

    chapter 2

--- hogwarts/chapters/chapter_2.py
+++ hogwarts/chapters/chapter_2.py
@@ -4,5 +4,9 @@ hogwarts_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, hogwarts_root)
     
 from utils.input_utils import *
+from universe.house import *
 from universe.character import *
 from chapters.chapter_1 import *
+
+def start_chapter_2(character):
+    assign_house(character,questions)
